1fbc068ed0a4d6afeed36fc28f34ca601953b325
Javier Salcedo <javierss.2603@gmail.com>
2021-11-02 15:43:05 -0500

    work environment settings

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/global.scss'
+
+const App = () => {
+    return (
+        <h1>Hola mundos, bienbenidos al curso practico de react!!</h1>
+    );
+}
+
+export default App;
